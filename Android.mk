a6bbad34a3d9147b5d59ee2d881b989ef900df2d
Adrian DC <radian.dc@gmail.com>
2017-04-18 23:13:52 +0200

    stlport: Use flo for 32 bits and flounder for 64bits
     * Restores support of libstlport for devices
        that are not part of the whitelist
    
    Change-Id: I6630de773ebc1523b6f8663a467705c4720b7290
    Signed-off-by: Adrian DC <radian.dc@gmail.com>

--- Android.mk
+++ Android.mk
@@ -13,7 +13,7 @@ STLPORT_WHITELIST := \
     shamu \
     tilapia \
 
-ifneq (,$(filter $(TARGET_DEVICE),$(STLPORT_WHITELIST)))
+#ifneq (,$(filter $(TARGET_DEVICE),$(STLPORT_WHITELIST)))
 
 LOCAL_PATH := $(call my-dir)
 
@@ -21,13 +21,19 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := libstlport
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 LOCAL_MODULE_SUFFIX := .so
-LOCAL_SRC_FILES := $(TARGET_DEVICE)/$(LOCAL_MODULE).so
 
 ifeq ($(TARGET_IS_64_BIT),true)
+
 LOCAL_MULTILIB := both
-LOCAL_SRC_FILES_32 := $(TARGET_DEVICE)/32/$(LOCAL_MODULE).so
+LOCAL_SRC_FILES := flounder/$(LOCAL_MODULE).so
+LOCAL_SRC_FILES_32 := flounder/32/$(LOCAL_MODULE).so
+
+else
+
+LOCAL_SRC_FILES := flo/$(LOCAL_MODULE).so
+
 endif
 
 include $(BUILD_PREBUILT)
 
-endif
+#endif
